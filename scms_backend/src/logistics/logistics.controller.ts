c376324b1bbf86f5775b74e8faf1b1bd757bb1e9
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2023-10-02 10:34:55 +0600

    Create basic `Get` APIs
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- scms_backend/src/logistics/logistics.controller.ts
+++ scms_backend/src/logistics/logistics.controller.ts
@@ -1,9 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('logistics')
 export class LogisticsController {
-	@Get()
-	testHello(): string {
-		return 'Hello from LogisticsController';
+	@Get('trucks')
+	getTrucks(): string {
+		return 'This action returns all cars';
+	}
+
+	@Get('trucks/:id')
+	getTruckById(@Param('id') id: number): string {
+		return 'This action returns a '+id+ ' truck';
+	}
+
+	@Get('trucks/:id/drivers')
+	getDriversByTruckId(@Param('id') id: number): string {
+		return 'This action returns drivers for truck '+id;
+	}
+
+	@Get('drivers')
+	getDrivers(): string {
+		return 'This action returns all drivers';
+	}
+
+	@Get('drivers/:id')
+	getDriverById(@Param('id') id: number): string {
+		return 'This action returns a driver '+id;
+	}
+
+	@Get('drivers/:id/trucks')
+	getTrucksByDriverId(@Param('id') id: number): string {
+		return 'This action returns trucks for driver '+id;
+	}
+
+	//Create APIs for the following:
+
+	@Post(trucks)
+	createTrucks(@Body() newTruck: Object): Object{
+		return newTruck;
 	}
 }
