17da353914ce4fa970630040f785d6fec17c662e
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-10-02 10:31:14 +0600

    admin init

---
+++ scms_backend/scms_backend/src/admin/admin.service.ts
@@ -0,0 +1,25 @@
+import { Body, Injectable } from '@nestjs/common';
+import { ProducerInfo } from './producer.dto';
+
+@Injectable()
+export class AdminService {
+  getHello(): string {
+    return 'Show All Product Producers!';
+  }
+  addProducer(producerInfo: ProducerInfo): object {
+    return {
+        id: producerInfo.id,
+        name: producerInfo.name,
+        email: producerInfo.email
+    }
+  }
+  searchProducer(name:string, id:number): string {
+    return `userid: ${id}\nusername: ${name}`;
+  }
+  updateProducer(id): string {
+    return `User id ${id} updated`
+  }
+  deleteProducer(id): string {
+    return `User id ${id} Deleted`
+  }
+}
