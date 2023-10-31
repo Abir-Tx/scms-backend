b5035a804eda0b2ebbc305462b00dca7d5c7df79
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-10-31 23:19:25 +0600

    Remove old routes and add usage of the new service
    This is a skeleton now just
    
    Signed-off-by: Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>

--- scms_backend/src/logistics/logistics.controller.ts
+++ scms_backend/src/logistics/logistics.controller.ts
@@ -1,34 +1,17 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { LogisticsService } from './logistics.service';
 
 @Controller('logistics')
 export class LogisticsController {
-  @Get('trucks')
-  getTrucks(): string {
-    return 'This action returns all cars';
+  constructor(private readonly logisticsService: LogisticsService) {}
+  // GET Transports
+  @Post()
+  createTransportRequest(@Body() requestData) {
+    return this.logisticsService.createTransportRequest(requestData);
   }
 
-  @Get('trucks/:id')
-  getTruckById(@Param('id') id: number): string {
-    return 'This action returns a ' + id + ' truck';
-  }
-
-  @Get('trucks/:id/drivers')
-  getDriversByTruckId(@Param('id') id: number): string {
-    return 'This action returns drivers for truck ' + id;
-  }
-
-  @Get('drivers')
-  getDrivers(): string {
-    return 'This action returns all drivers';
-  }
-
-  @Get('drivers/:id')
-  getDriverById(@Param('id') id: number): string {
-    return 'This action returns a driver ' + id;
-  }
-
-  @Get('drivers/:id/trucks')
-  getTrucksByDriverId(@Param('id') id: number): string {
-    return 'This action returns trucks for driver ' + id;
+  @Get()
+  getAvailableTransportOptions() {
+    return this.logisticsService.getAvailableTransportOptions();
   }
 }
