07726a8c899e25cd729c20c37780e788fc2267dc
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-11-02 15:33:44 +0600

    Add driver, transport and shipment entity
    Signed-off-by: Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>

---
+++ scms_backend/src/logistics/entities/transport.entity.ts
@@ -0,0 +1,28 @@
+// transport.entity.ts
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from 'typeorm';
+import { Driver } from './driver.entity'; // Assuming you have a driver entity
+import { Shipment } from './shipment.entity';
+
+@Entity()
+export class Transport {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  source: string;
+
+  @Column()
+  destination: string;
+
+  @Column({ type: 'date' })
+  requestedDate: Date;
+
+  @Column()
+  status: string;
+
+  @ManyToOne(() => Driver, (driver) => driver.transports)
+  driver: Driver;
+
+  @OneToMany(() => Shipment, (shipment) => shipment.transport)
+  shipments: Shipment[];
+}
