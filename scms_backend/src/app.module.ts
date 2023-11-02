109fbedd7621a9d57ebbf5db5497733737dd1ea5
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-11-02 13:41:01 +0600

    Add typeorm config for pg
    Signed-off-by: Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>

--- scms_backend/src/app.module.ts
+++ scms_backend/src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LogisticsModule } from './logistics/logistics.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [LogisticsModule],
+  imports: [
+    LogisticsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'abir',
+      password: '12918',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
