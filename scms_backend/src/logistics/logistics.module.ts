22540eea43aaf81ec2ad2203af9f3e09d07872e6
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-11-02 15:53:19 +0600

    Add the driver service and enable feature in logModule
    Signed-off-by: Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>

--- scms_backend/src/logistics/logistics.module.ts
+++ scms_backend/src/logistics/logistics.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { LogisticsController } from './logistics.controller';
 import { LogisticsService } from './logistics.service';
+import { DriverService } from './services/driver.service';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import { Driver } from './entities/driver.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Driver])],
   controllers: [LogisticsController],
-  providers: [LogisticsService],
+  providers: [LogisticsService, DriverService],
 })
 export class LogisticsModule {}
