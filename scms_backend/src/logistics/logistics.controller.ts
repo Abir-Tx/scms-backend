c0f1c2084ab80b65f90939ffede48d4f6871bc61
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-11-02 15:55:12 +0600

    Add get all drivers api
    Signed-off-by: Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>

--- scms_backend/src/logistics/logistics.controller.ts
+++ scms_backend/src/logistics/logistics.controller.ts
@@ -1,10 +1,14 @@
 // Logistics Manager controller
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { LogisticsService } from './logistics.service';
+import { DriverService } from './services/driver.service';
 
 @Controller('logistics')
 export class LogisticsController {
-  constructor(private readonly logisticsService: LogisticsService) {}
+  constructor(
+    private readonly driverService: DriverService,
+    private readonly logisticsService: LogisticsService,
+  ) {}
   // GET Transports
   @Post()
   createTransportRequest(@Body() requestData) {
@@ -14,5 +18,14 @@ export class LogisticsController {
   @Get()
   getAvailableTransportOptions() {
     return this.logisticsService.getAvailableTransportOptions();
+  }
+
+  // Drivers
+  @Get('drivers')
+  getDrivers() {
+    // Call the DriverService method to get a list of drivers
+    const drivers = this.driverService.findAll(); // Assuming you have a findAll method
+
+    return drivers;
   }
 }
