a5a2dc189ff376693ee5c21177fad8f07171c797
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-11-02 16:34:21 +0600

    Remove unnecessary comments

--- scms_backend/src/logistics/logistics.controller.ts
+++ scms_backend/src/logistics/logistics.controller.ts
@@ -33,30 +33,27 @@ export class LogisticsController {
   // Drivers
   @Get('drivers')
   getDrivers() {
-    // Call the DriverService method to get a list of drivers
-    const drivers = this.driverService.findAll(); // Assuming you have a findAll method
+    const drivers = this.driverService.findAll();
 
     return drivers;
   }
 
   @Get('drivers/:id')
   getDriver(@Param('id') id: string) {
-    const driver = this.driverService.findById(parseInt(id)); // Assuming you have a findById method
+    const driver = this.driverService.findById(parseInt(id));
 
     return driver;
   }
 
   @Post('drivers')
   async addDriver(@Body() driverData) {
-    // Call the DriverService method to create a new driver
-    const newDriver = await this.driverService.create(driverData); // Assuming you have a create method in your DriverService
+    const newDriver = await this.driverService.create(driverData);
     return newDriver;
   }
 
   @Delete('drivers/:id')
   async deleteDriver(@Param('id') id: string, @Res() response) {
     try {
-      // Call the DriverService method to delete the driver
       await this.driverService.remove(parseInt(id));
       response.status(200).json({ message: 'Driver successfully deleted' });
     } catch (error) {
@@ -75,7 +72,7 @@ export class LogisticsController {
       );
       return updatedDriver;
     } catch (error) {
-      return error; // You can return the error response here
+      return error;
     }
   }
 }
