bfa05dfe4cb215a52e3cebaf5acaa55811bb80ad
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-11-02 16:11:58 +0600

    Add driver deletion api with exception handling
    Signed-off-by: Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>

--- scms_backend/src/logistics/logistics.controller.ts
+++ scms_backend/src/logistics/logistics.controller.ts
@@ -1,5 +1,5 @@
 // Logistics Manager controller
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Res } from '@nestjs/common';
 import { LogisticsService } from './logistics.service';
 import { DriverService } from './services/driver.service';
 
@@ -34,5 +34,18 @@ export class LogisticsController {
     // Call the DriverService method to create a new driver
     const newDriver = await this.driverService.create(driverData); // Assuming you have a create method in your DriverService
     return newDriver;
+  }
+
+  @Delete('drivers/:id')
+  async deleteDriver(@Param('id') id: string, @Res() response) {
+    try {
+      // Call the DriverService method to delete the driver
+      await this.driverService.remove(parseInt(id));
+      response.status(200).json({ message: 'Driver successfully deleted' });
+    } catch (error) {
+      response
+        .status(500)
+        .json({ message: error.message || 'Something went wrong' });
+    }
   }
 }
