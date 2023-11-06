8d7442c74bb3fc60fc25c0f9bfc8d37ec55beee9
bakibillahrahat <bakibillahrahat@protonmail.com>
2023-11-06 21:59:47 +0600

    connect typorm & postgresql

--- scms_backend/src/admin/admin.module.ts
+++ scms_backend/src/admin/admin.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [AuthModule],
--- scms_backend/src/app.module.ts
+++ scms_backend/src/app.module.ts
@@ -1,12 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdminModule } from './admin/admin.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './admin/auth/auth.module';
-import { AdminModule } from './admin/admin.module';
+import { AuthModule } from './auth/auth.module';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+import { UserService } from './user/user.service';
+import { TypeormModule } from './typeorm/typeorm.module';
 
 @Module({
-  imports: [AuthModule, AdminModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule,
+    AdminModule,
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      port: 5434,
+      username: 'postgres',
+      password: 'proGRAMMER6996!@#',
+      database: 'scms',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    TypeormModule,
+  ],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
