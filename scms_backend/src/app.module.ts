de6ddd07cd2a4aad769ae632d7f0b51e7be14702
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-11-09 17:42:16 +0600

    Add `dotenv` package and make use of it

--- scms_backend/src/app.module.ts
+++ scms_backend/src/app.module.ts
@@ -6,16 +6,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Driver } from './logistics/entities/driver.entity';
 import { Transport } from './logistics/entities/transport.entity';
 import { Shipment } from './logistics/entities/shipment.entity';
+require('dotenv').config();
+
 @Module({
   imports: [
     LogisticsModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.PG_HOST || 'localhost',
+      host: process.env.PG_HOST,
       port: parseInt(process.env.PG_PORT) || 5432,
       username: process.env.PG_USER,
       password: process.env.PG_PASSWORD,
-      database: process.env.PG_DATABASE || 'test',
+      database: process.env.PG_DATABASE,
       entities: [Transport, Driver, Shipment],
       synchronize: true,
     }),
