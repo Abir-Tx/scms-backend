6b3b13d1c79610b430164a4d4e3226904013ffcc
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2024-03-18 16:46:55 +0600

    Add welcome msg to api root

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Welcome to SCMS API Created By Mushfiqur Rahman Abir. Get the API documentation at https://documenter.getpostman.com/view/21420955/2s9YXk3gYw';
   }
 }
